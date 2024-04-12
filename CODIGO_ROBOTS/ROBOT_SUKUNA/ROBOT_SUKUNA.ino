4beb181ab137448bae0b896dea029001173427e2
AlfredoSandoval6 <alfredo.sandoval7616@gmail.com>
2024-04-12 05:29:34 -0700

    Añadidas rutinas de búsqueda y control para las etapas finales de códigos a usar en RANGO y SUKUNA.

--- CODIGO_ROBOTS/ROBOT_SUKUNA/ROBOT_SUKUNA.ino
+++ CODIGO_ROBOTS/ROBOT_SUKUNA/ROBOT_SUKUNA.ino
@@ -1,46 +1,31 @@
+#include <Servo.h>
+Servo servoMotor1;
+
 #define MOTL 4
 #define PWML 6
 #define MOTR 3
 #define PWMR 5
 
 #define SENLL 19
+#define SENDL 9
 #define SENL 18
 #define SENF 17
 #define SENR 16
+#define SENDR 11
 #define SENLR 15
-#define SENPL A7
-#define SENPR A6
-#define NUM_S 5
+#define SENPL A6
+#define SENPR A7
+#define NUM_S 7
 #define ARR 2
-#define PSGY1 8
-#define PSGY2 7
 
-#define KP 12
-#define KD 10
-#define SETPOINT 30
+#define KP 6
+#define KD 3
+#define SETPOINT 40
 #define MAX_SPEED 250
 #define NO_SPEED 0
 
-byte vSteARR = 0;
-byte sens = 0;
-byte ste = 0;
-
-void sgy1();
-void sgy2();
-void sgy3();
-void sgy4();
-void lectura();
-void posAnalisis();
-void bus_leo();
-void control();
-int ponderacion();
-void izquierda(byte PWMI, byte PWMD, int T);
-void derecha(byte PWMI, byte PWMD, int T);
-void adelante(byte PWMI, byte PWMD, int T);
-void atras(byte PWMI, byte PWMD, int T);
-void paro(int T);
-void (*SGTY[])() = {sgy1, sgy2, sgy3, sgy4};
-
+int sens = 0;
+byte ste = false;
 void setup(){
 
     pinMode(MOTL, OUTPUT); pinMode(MOTR, OUTPUT);
@@ -48,84 +33,39 @@ void setup(){
     pinMode(ARR, INPUT);
     digitalWrite(MOTL, LOW); digitalWrite(MOTR, LOW);
     analogWrite(PWML, 0); analogWrite(PWMR, 0);
+    servoMotor1.attach(10);
     //Serial.begin(9600);
 
 }
 
 void loop(){
-
-    if(digitalRead(ARR)) selSgy();
-
-    if(!digitalRead(ARR)){
+    if(!ste){
         paro(0);
+        servoMotor1.write(0);
+        ste = true;
     }
-
+    if(!digitalRead(ARR)) paro(0);
+    if(digitalRead(ARR)){
+        servoMotor1.write(90);
+        ste = false;
+    }
+    while(digitalRead(ARR)){
+        posAnalisis();
+    }
 }
 
 void lectura(){
 
-    (digitalRead(SENLL)) ? (sens |= (1 << 0)) : (sens &= ~(1 << 0));
-    (digitalRead(SENL)) ? (sens |= (1 << 1)) : (sens &= ~(1 << 1));
-    (digitalRead(SENF)) ? (sens |= (1 << 2)) : (sens &= ~(1 << 2));
-    (digitalRead(SENR)) ? (sens |= (1 << 3)) : (sens &= ~(1 << 3));
-    (digitalRead(SENLR)) ? (sens |= (1 << 4)) : (sens &= ~(1 << 4));
-    if(analogRead(SENPL) <= 100) sens |= (1 << 5); else sens &= ~(1 << 5);
-    if(analogRead(SENPR) <= 100) sens |= (1 << 6); else sens &= ~(1 << 6);
+    (!digitalRead(SENLL)) ? (sens |= (1 << 0)) : (sens &= ~(1 << 0));
+    (!digitalRead(SENDL)) ? (sens |= (1 << 1)) : (sens &= ~(1 << 1));
+    (!digitalRead(SENL)) ? (sens |= (1 << 2)) : (sens &= ~(1 << 2));
+    (!digitalRead(SENF)) ? (sens |= (1 << 3)) : (sens &= ~(1 << 3));
+    (!digitalRead(SENR)) ? (sens |= (1 << 4)) : (sens &= ~(1 << 4));
+    (!digitalRead(SENDR)) ? (sens |= (1 << 5)) : (sens &= ~(1 << 5));
+    (!digitalRead(SENLR)) ? (sens |= (1 << 6)) : (sens &= ~(1 << 6));
+    (analogRead(SENPL) <= 700) ? (sens |= (1 << 7)) : (sens &= ~(1 << 7));
+    (analogRead(SENPR) <= 700) ? (sens |= (1 << 8)) : (sens &= ~(1 << 8));
 
-}
-
-void selSgy(){
-
-    byte sgy = 0;
-
-    (digitalRead(PSGY1)) ? (sgy |= (1 << 0)) : (sgy &= ~(1 << 0));
-    (digitalRead(PSGY2)) ? (sgy |= (1 << 1)) : (sgy &= ~(1 << 1));
-
-    (*SGTY[sgy])();
-
-    while(digitalRead(ARR)) posAnalisis();
-
-}
-
-
-void sgy1(){
-
-}
-
-void sgy2(){
-
-    unsigned long taim = millis();
-
-    adelante(87, 50, 0);
-    do{
-        lectura();
-        if(sens || (!digitalRead(ARR))) return;
-    }while((millis() - taim) <= 500);
-
-}
-
-void sgy3(){
-
-    unsigned long taim = millis();
-
-    adelante(50, 87, 0);
-    do{
-        lectura();
-        if(sens || (!digitalRead(ARR))) return;
-    }while((millis() - taim) <= 500);
-
-}
-
-void sgy4(){
-
-    unsigned long taim = millis();
-
-    adelante(25, 25, 0);
-    do{
-        lectura();
-        if(sens || (!digitalRead(ARR))) return;
-    }while((millis() - taim) <= 500);
-    
 }
 
 void posAnalisis(){
@@ -133,21 +73,18 @@ void posAnalisis(){
     lectura();
     if(!sens) bus_leo();
 
-    if(bitRead(sens, 5) || bitRead(sens, 6)){
-        if(bitRead(sens, 5) && bitRead(sens, 6)){
-            atras(MAX_SPEED, MAX_SPEED, 75);
-            izquierda(240, 240, 100);
-        } else{
-            if(bitRead(sens, 5)){
-                atras(MAX_SPEED, MAX_SPEED, 75);
-                izquierda(240, 240, 75);
-            } else if(bitRead(sens, 6)){
-                atras(MAX_SPEED, MAX_SPEED, 75);
-                derecha(240, 240, 75);
-            }
+    if(bitRead(sens, 7) || bitRead(sens, 8)){
+        if(bitRead(sens, 7) && bitRead(sens, 8)){
+            atras(MAX_SPEED, MAX_SPEED, 100);
+            izquierda(240, 240, 125);
+        } else if(bitRead(sens, 7)){
+            atras(MAX_SPEED, MAX_SPEED, 100);
+            izquierda(240, 240, 125);
+        } else if(bitRead(sens, 8)){
+            atras(MAX_SPEED, MAX_SPEED, 100);
+            derecha(240, 240, 125);
         }
-        
-    } else if(sens & 0B00011111) control();
+    } else if(sens & 0B0000000001111111) control();
 
 }
 
@@ -155,34 +92,35 @@ void bus_leo(){
 
     unsigned long taim = millis();
 
-    adelante(150, 150, 0);
+    adelante(40, 40, 0);
     do{
+
         lectura();
         if(sens || (!digitalRead(ARR))) return;
+
     }while((millis() - taim) <= 100);
 
     taim = millis();
     paro(0);
     do{
+
         lectura();
         if(sens || (!digitalRead(ARR))) return;
-    }while((millis() - taim) <= 1000);
+
+    }while((millis() - taim) <= 3000);
 
 }
 
 void control(){
-    //Serial.println(F("CONTROL"));
-    static int ultEntrada;
+
+    static int ultEntrada = 0;
     int posicion = ponderacion();
     if(!sens || (!digitalRead(ARR))) return;
     int error = SETPOINT - posicion;
     int dEntrada = posicion - ultEntrada;
 
     int salida = (error * KP) - (dEntrada * KD);
-
-    if(salida > MAX_SPEED) salida = MAX_SPEED;
-    else if(salida < -MAX_SPEED) salida = -MAX_SPEED;
-    //Serial.println(salida);
+    
     if(salida < 0){
         salida *= -1;
         //izquierda(salida, 0, 2);
@@ -218,8 +156,7 @@ int ponderacion(){
         amnt += 10;
     }
     amnt_det /= n_det;
-    //Serial.print(amnt_det);
-    //Serial.print("\t");
+    
   return amnt_det;
 }
 
